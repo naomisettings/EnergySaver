a0ca4f6d89d460fa3ab2f67d9a51ac9ad86a0cf3
Angels <marmol.samper.maria.angeles@alumnat.copernic.cat>
2021-01-30 18:27:32 +0100

    Imatges

--- app/src/main/java/cat/copernic/johan/energysaver/AuthActivity.kt
+++ app/src/main/java/cat/copernic/johan/energysaver/AuthActivity.kt
@@ -170,7 +170,7 @@ class AuthActivity : AppCompatActivity(), View.OnClickListener {
                 //si falla l'acces
                 val builder = AlertDialog.Builder(this)
                 builder.setTitle("Error")
-                builder.setMessage("Error amb l'autentificació, usuario o contrasenya no vàlids ")
+                builder.setMessage("Error amb l'autentificació, usuari o contrasenya no vàlids ")
                 builder.setPositiveButton("Acceptar", null)
                 val dialog: AlertDialog = builder.create()
                 dialog.show()
