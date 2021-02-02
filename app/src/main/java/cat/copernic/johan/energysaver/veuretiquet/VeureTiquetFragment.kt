19b926f5b9eb5cfb5cdb0a6230b2d3258dac5a1e
sellare.feiner.raimon <sellares.feiner.raimon@alumnat.copernic.cat>
2021-02-02 12:59:22 +0100

    Diseny obrir tiquet i veure tiquet

--- app/src/main/java/cat/copernic/johan/energysaver/veuretiquet/VeureTiquetFragment.kt
+++ app/src/main/java/cat/copernic/johan/energysaver/veuretiquet/VeureTiquetFragment.kt
@@ -5,15 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import cat.copernic.johan.energysaver.R
+import cat.copernic.johan.energysaver.databinding.FragmentVeureBinding
 
 
-class veureFragment : Fragment() {
+class VeureTiquetFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_veure, container, false)
+        val binding : FragmentVeureBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_veure, container, false)
+        return binding.root
     }
 }
