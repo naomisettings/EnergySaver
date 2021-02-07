af3396beb56ac57e25632b0e847f65a942eefb68
sellare.feiner.raimon <sellares.feiner.raimon@alumnat.copernic.cat>
2021-02-07 16:35:26 +0100

    Fragments medalles individuals

---
+++ app/src/main/java/cat/copernic/johan/energysaver/medalles/Medalla2Fragment.kt
@@ -0,0 +1,23 @@
+package cat.copernic.johan.energysaver.medalles
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import cat.copernic.johan.energysaver.R
+import cat.copernic.johan.energysaver.databinding.FragmentObrirBinding
+
+class Medalla2Fragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding: FragmentObrirBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_medalla2, container, false)
+
+        return binding.root
+    }
+}
