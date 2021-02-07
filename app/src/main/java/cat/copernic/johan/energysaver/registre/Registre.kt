58600a3b670e6777e61e69d292ff633144bc168c
Angels <marmol.samper.maria.angeles@alumnat.copernic.cat>
2021-02-07 18:32:05 +0100

    Refer Navigation Drawer, contradiccions Git. Navigation Path

--- app/src/main/java/cat/copernic/johan/energysaver/registre/Registre.kt
+++ app/src/main/java/cat/copernic/johan/energysaver/registre/Registre.kt
@@ -1,13 +1,24 @@
 package cat.copernic.johan.energysaver.registre
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import cat.copernic.johan.energysaver.MainActivity
 import cat.copernic.johan.energysaver.R
+import cat.copernic.johan.energysaver.databinding.ActivityRegistreBinding
+
+private lateinit var binding: ActivityRegistreBinding
 
 class Registre : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registre)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_registre)
+
+        binding.btnConfirmarRegistre.setOnClickListener { val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent) }
     }
 
 }
