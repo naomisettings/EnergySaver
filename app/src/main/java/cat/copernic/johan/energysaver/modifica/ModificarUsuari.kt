2508ff43f6ed953206701543887dd66ac4e71ceb
Angels <marmol.samper.maria.angeles@alumnat.copernic.cat>
2021-02-09 10:57:27 +0100

    Navegacio Pantalla Modificar usuari

--- app/src/main/java/cat/copernic/johan/energysaver/modifica/ModificarUsuari.kt
+++ app/src/main/java/cat/copernic/johan/energysaver/modifica/ModificarUsuari.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import cat.copernic.johan.energysaver.R
 import cat.copernic.johan.energysaver.databinding.FragmentModificarUsuariBinding
 
@@ -18,6 +19,21 @@ class ModificarUsuari : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
         val binding: FragmentModificarUsuariBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_modificar_usuari,container, false)
+
+        binding.btnConfirmarModificar.setOnClickListener {
+            view:View ->
+            view.findNavController().navigate(R.id.action_modificarUsuari_to_menuPrincipalFragment)
+        }
+
+        binding.btnEnergiesModificar.setOnClickListener {
+            view:View ->
+            view.findNavController().navigate(R.id.action_modificarUsuari_to_seleccionarEnergiaFragment)
+        }
+
+        binding.btnTancarModificar.setOnClickListener {
+            view:View ->
+            view.findNavController().navigate(R.id.action_modificarUsuari_to_authActivity)
+        }
         return binding.root
     }
 
