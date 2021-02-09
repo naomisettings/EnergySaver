65b911ae2a6f1cbda869db0b1301c7cee703092f
Angels <marmol.samper.maria.angeles@alumnat.copernic.cat>
2021-02-09 12:08:09 +0100

    Pantalla registre usuari Firebase v.2

--- app/src/main/java/cat/copernic/johan/energysaver/registre/Registre.kt
+++ app/src/main/java/cat/copernic/johan/energysaver/registre/Registre.kt
@@ -3,11 +3,14 @@ package cat.copernic.johan.energysaver.registre
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import cat.copernic.johan.energysaver.MainActivity
 import cat.copernic.johan.energysaver.R
 import cat.copernic.johan.energysaver.databinding.ActivityRegistreBinding
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
+
 
 
 
@@ -16,7 +19,18 @@ import com.google.firebase.ktx.Firebase
 class Registre : AppCompatActivity() {
     private lateinit var binding: ActivityRegistreBinding
 
+    val db = FirebaseFirestore.getInstance()
 
+    var nom: String =""
+    var cognoms: String =""
+    var mail: String = ""
+    var nickname : String =""
+    var adreca: String = ""
+    var telefon: String = ""
+    var contrasenya: String = ""
+
+
+    
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,9 +39,21 @@ class Registre : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_registre)
 
         binding.btnConfirmarRegistre.setOnClickListener {
-            //db.collection
+
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent) }
+    }
+
+    fun recollirUsuari(view:View){
+        binding.apply {
+            nom = editTextNom.text.toString()
+            cognoms = editTextCognoms.text.toString()
+            mail = editTextTextEmailAddress.text.toString()
+            nickname = editTextNickName.text.toString()
+            adreca = editTextAdreca.text.toString()
+            telefon = editTextTelefon.text.toString()
+            contrasenya = editTextContrasenyaRegistre.text.toString()
+        }
     }
 
 }
