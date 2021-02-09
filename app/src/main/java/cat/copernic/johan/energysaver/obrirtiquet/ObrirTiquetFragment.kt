016d6500035a9622255aac832ba75990a73fb3b8
sellare.feiner.raimon <sellares.feiner.raimon@alumnat.copernic.cat>
2021-02-09 10:36:03 +0100

    SnackBar obrir tiquet

--- app/src/main/java/cat/copernic/johan/energysaver/obrirtiquet/ObrirTiquetFragment.kt
+++ app/src/main/java/cat/copernic/johan/energysaver/obrirtiquet/ObrirTiquetFragment.kt
@@ -1,6 +1,8 @@
 package cat.copernic.johan.energysaver.obrirtiquet
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +11,7 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import cat.copernic.johan.energysaver.R
 import cat.copernic.johan.energysaver.databinding.FragmentObrirBinding
+import com.google.android.material.snackbar.Snackbar
 
 class ObrirTiquetFragment : Fragment() {
 
@@ -21,20 +24,21 @@ class ObrirTiquetFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_obrir, container, false)
 
         binding.bttnConfirmarTiquet.setOnClickListener{
-            rebreDades()
+            rebreDades(it)
         }
 
         return binding.root
     }
 
-    fun rebreDades(){
+    fun rebreDades(view: View){
         binding.apply{
             titol = editTextTemaTiquet.text.toString()
             descripcio = editTxtDescripcioTiquet.text.toString()
         }
 
         if (titol.isEmpty() || descripcio.isEmpty()){
-            Toast.makeText(context, R.string.campsBuitsToastObrirTiquet, Toast.LENGTH_SHORT).show()
+            Log.w("ObrirTiquetFragment", "Entra fun rebre dades")
+            Snackbar.make(view,R.string.campsBuitsToastObrirTiquet, Snackbar.LENGTH_LONG).show()
         }else{
 
             print("firebase")
