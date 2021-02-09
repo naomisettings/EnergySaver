5b4c3b61c7da34b4deb6b0b3d12b949dc2a8d78e
sellare.feiner.raimon <sellares.feiner.raimon@alumnat.copernic.cat>
2021-02-09 11:00:59 +0100

    Primers passos firestore

--- app/src/main/java/cat/copernic/johan/energysaver/obrirtiquet/ObrirTiquetFragment.kt
+++ app/src/main/java/cat/copernic/johan/energysaver/obrirtiquet/ObrirTiquetFragment.kt
@@ -1,5 +1,6 @@
 package cat.copernic.johan.energysaver.obrirtiquet
 
+import android.content.ContentValues.TAG
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
@@ -12,36 +13,40 @@ import androidx.databinding.DataBindingUtil
 import cat.copernic.johan.energysaver.R
 import cat.copernic.johan.energysaver.databinding.FragmentObrirBinding
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class ObrirTiquetFragment : Fragment() {
 
-    private lateinit var binding : FragmentObrirBinding
+    private lateinit var binding: FragmentObrirBinding
+    val db = Firebase.firestore
     var titol: String = ""
     var descripcio: String = ""
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_obrir, container, false)
 
-        binding.bttnConfirmarTiquet.setOnClickListener{
+        binding.bttnConfirmarTiquet.setOnClickListener {
             rebreDades(it)
         }
 
         return binding.root
     }
 
-    fun rebreDades(view: View){
-        binding.apply{
+    fun rebreDades(view: View) {
+        binding.apply {
             titol = editTextTemaTiquet.text.toString()
             descripcio = editTxtDescripcioTiquet.text.toString()
         }
 
-        if (titol.isEmpty() || descripcio.isEmpty()){
+        if (titol.isEmpty() || descripcio.isEmpty()) {
             Log.w("ObrirTiquetFragment", "Entra fun rebre dades")
-            Snackbar.make(view,R.string.campsBuitsToastObrirTiquet, Snackbar.LENGTH_LONG).show()
-        }else{
-
-            print("firebase")
+            Snackbar.make(view, R.string.campsBuitsToastObrirTiquet, Snackbar.LENGTH_LONG).show()
+        } else {
+            print("firestore")
 
         }
 
