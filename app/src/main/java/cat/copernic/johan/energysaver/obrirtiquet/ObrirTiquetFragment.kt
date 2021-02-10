a3506cd9a758ad74b90b033e5a5f14cd569c4df7
sellare.feiner.raimon <sellares.feiner.raimon@alumnat.copernic.cat>
2021-02-10 16:47:58 +0100

    dades tiquets firestore

--- app/src/main/java/cat/copernic/johan/energysaver/obrirtiquet/ObrirTiquetFragment.kt
+++ app/src/main/java/cat/copernic/johan/energysaver/obrirtiquet/ObrirTiquetFragment.kt
@@ -13,7 +13,7 @@ import cat.copernic.johan.energysaver.R
 import cat.copernic.johan.energysaver.databinding.FragmentObrirBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import java.text.SimpleDateFormat
 import java.util.*
@@ -21,7 +21,7 @@ import java.util.*
 class ObrirTiquetFragment : Fragment() {
 
     private lateinit var binding: FragmentObrirBinding
-    val db = Firebase.firestore
+    val db = FirebaseFirestore.getInstance()
     var titol: String = ""
     var descripcio: String = ""
 
@@ -31,6 +31,7 @@ class ObrirTiquetFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_obrir, container, false)
 
+        //Botó confirmar que truca a la funció per inserir dades al firestore
         binding.bttnConfirmarTiquet.setOnClickListener {
             rebreDades(it)
         }
@@ -40,24 +41,32 @@ class ObrirTiquetFragment : Fragment() {
 
     @SuppressLint("SimpleDateFormat")
     fun rebreDades(view: View) {
+
+        //Agafar dades del editText titol i descripció
         binding.apply {
             titol = editTextTemaTiquet.text.toString()
             descripcio = editTxtDescripcioTiquet.text.toString()
         }
-
+        //Comprova que els camps esitguin emplenats
         if (titol.isEmpty() || descripcio.isEmpty()) {
             Log.w("ObrirTiquetFragment", "Entra fun rebre dades")
             Snackbar.make(view, R.string.campsBuitsToastObrirTiquet, Snackbar.LENGTH_LONG).show()
         } else {
 
+            //Guarda les dades del usuari connectat a la constant user
             val user = Firebase.auth.currentUser
+
+            //Guarda el mail del usuari que ha fet login
             val mail = user?.email.toString()
 
+            //Consulta per extreure el nickname per guardar-lo al document tiquet
+            /*
             val usuaris = db.collection("usuaris")
             val query = usuaris.whereEqualTo("mail", mail).get()
-                .addOnSuccessListener { documents ->
-                    for (document in documents) {
-                        Log.d(TAG, "${document.id} => ${document.data}")
+                .addOnSuccessListener {
+                        document ->
+                    if (document != null){
+                      // document.getString("nickname")
                     }
 
                 }
@@ -65,12 +74,18 @@ class ObrirTiquetFragment : Fragment() {
                     Log.w(TAG, "Error getting documents: ", exception)
                 }
 
+           Log.i("consulta",query.toString())
+
+             */
+
+            //Extreu la data i hora del sistema per guardar al document tiquet
             val data = Calendar.getInstance().time
             val formatterdt = SimpleDateFormat("yyyy.MM.dd")
             val formatterhr = SimpleDateFormat("HH:mm:ss")
             val formatedDate = formatterdt.format(data)
             val formatedHour = formatterhr.format(data)
 
+            //Map per fer l'insert
             val tiquet = hashMapOf(
                 "mail" to mail,
                 "nickname" to " ",
@@ -80,10 +95,13 @@ class ObrirTiquetFragment : Fragment() {
                 "descripcio" to descripcio
             )
 
+            //Neteja dels camps tema i descripció
             binding.apply {
                 editTextTemaTiquet.text.clear()
                 editTxtDescripcioTiquet.text.clear()
             }
+
+            //Incerció a la col·lecció tiquet
             db.collection("tiquet")
                 .add(tiquet)
                 .addOnSuccessListener { documentReference ->
