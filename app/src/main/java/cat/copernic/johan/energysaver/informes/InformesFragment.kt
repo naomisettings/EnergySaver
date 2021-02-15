837233f90050f23fa3593b23446e231351cb4a18
Sergio <lopez.castro.sergio@alumnat.copernic.cat>
2021-02-15 19:20:56 +0100

    Recollida de dades de la base de dades pels informes

--- app/src/main/java/cat/copernic/johan/energysaver/informes/InformesFragment.kt
+++ app/src/main/java/cat/copernic/johan/energysaver/informes/InformesFragment.kt
@@ -13,9 +13,17 @@ import cat.copernic.johan.energysaver.seleccio.Energies
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
+import java.text.SimpleDateFormat
+import java.util.*
 
 class InformesFragment : Fragment() {
     private val db = FirebaseFirestore.getInstance()
+
+    var aiguaGastada = 0
+    var llumGastada = 0
+    var gasGastat = 0
+    var gasoilGastat = 0
+    var data = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -27,12 +35,16 @@ class InformesFragment : Fragment() {
     fun omplirDades(){
         val user = Firebase.auth.currentUser
         val mail = user?.email.toString()
+
         val energies = db.collection("entrarEnergia")
         val query = energies.whereEqualTo("mail", mail).get()
             .addOnSuccessListener { document ->
                 if (!document.isEmpty) {
                     val dadesEnergia = document.toObjects(DadesEnergia::class.java)
-
+                    aiguaGastada = dadesEnergia[0].aiguaGastats
+                    llumGastada = dadesEnergia[0].llumGastats
+                    gasGastat = dadesEnergia[0].gasGastats
+                    gasoilGastat = dadesEnergia[0].gasoilGastats
                 }
             }
     }
