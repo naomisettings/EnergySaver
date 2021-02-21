a0f3333e5188e7d640a30db35f0235bd5bd1c030
sellare.feiner.raimon <sellares.feiner.raimon@alumnat.copernic.cat>
2021-02-21 20:46:35 +0100

    Començar a utilitzar camerax

--- app/src/main/java/cat/copernic/johan/energysaver/MainActivity.kt
+++ app/src/main/java/cat/copernic/johan/energysaver/MainActivity.kt
@@ -1,17 +1,27 @@
 package cat.copernic.johan.energysaver
 
-
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.*
+import cat.copernic.johan.energysaver.databinding.FragmentObrirBinding
 import com.google.android.material.navigation.NavigationView
+import java.io.File
+import java.util.concurrent.ExecutorService
+
+typealias LumaListener = (luma: Double) -> Unit
 
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var outputDirectory: File
+    private lateinit var binding: FragmentObrirBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,24 +29,37 @@ class MainActivity : AppCompatActivity(){
         setContentView(R.layout.activity_main)
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView :NavigationView = findViewById(R.id.nav_view)
+        val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = this.findNavController(R.id.nav_host_fragment)
 
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.seleccionarEnergiaFragment, R.id.informesFragment, R.id.modificarFragment,
                 R.id.menuPrincipalFragment, R.id.obrirFragment, R.id.Sortir, R.id.registre,
-                R.id.veureFragment,R.id.menuEnergies
+                R.id.veureFragment, R.id.menuEnergies
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+        // Set up the listener for take photo button
 
-       /* val fragmentVeureTiquet = VeureTiquetFragment()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, fragmentVeureTiquet).commit()
- */
+       /* imgBttnCamera.setOnClickListener {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+                )
+            }
+
+            takePhoto()
+            outputDirectory = getOutputDirectory()
+
+            cameraExecutor = Executors.newSingleThreadExecutor()
+        }
+        */
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -45,8 +68,36 @@ class MainActivity : AppCompatActivity(){
     }
 
 
+    private fun takePhoto() {}
 
+    fun startCamera() {}
 
+    fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
 }
+
 
 
