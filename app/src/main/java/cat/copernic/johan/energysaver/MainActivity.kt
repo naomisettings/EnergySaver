2aa481a024cae9c591a819bef5ef910f55444d97
sellare.feiner.raimon <sellares.feiner.raimon@alumnat.copernic.cat>
2021-02-22 09:06:37 +0100

    Crear canviar fragment obrir tiquet per una activity (La cameraX, ha de ser una activity)

--- app/src/main/java/cat/copernic/johan/energysaver/MainActivity.kt
+++ app/src/main/java/cat/copernic/johan/energysaver/MainActivity.kt
@@ -1,28 +1,16 @@
 package cat.copernic.johan.energysaver
 
-import android.Manifest
-import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.content.ContextCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.*
-import cat.copernic.johan.energysaver.databinding.FragmentObrirBinding
 import com.google.android.material.navigation.NavigationView
-import java.io.File
-import java.util.concurrent.ExecutorService
-
-typealias LumaListener = (luma: Double) -> Unit
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var cameraExecutor: ExecutorService
-    private lateinit var outputDirectory: File
-    private lateinit var binding: FragmentObrirBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,23 +30,6 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        // Set up the listener for take photo button
-
-        /* imgBttnCamera.setOnClickListener {
-            if (allPermissionsGranted()) {
-                startCamera()
-            } else {
-                ActivityCompat.requestPermissions(
-                    this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
-                )
-            }
-
-            takePhoto()
-            outputDirectory = getOutputDirectory()
-
-            cameraExecutor = Executors.newSingleThreadExecutor()
-        }
-        */
 
     }
 
@@ -66,38 +37,9 @@ class MainActivity : AppCompatActivity() {
         val navController = this.findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration)
     }
-
-
-    private fun takePhoto() {}
-
-    fun startCamera() {}
-
-    fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(
-            baseContext, it
-        ) == PackageManager.PERMISSION_GRANTED
-    }
-
-    fun getOutputDirectory(): File {
-        val mediaDir = externalMediaDirs.firstOrNull()?.let {
-            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
-        }
-        return if (mediaDir != null && mediaDir.exists())
-            mediaDir else filesDir
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        cameraExecutor.shutdown()
-    }
-
-    companion object {
-        private const val TAG = "CameraXBasic"
-        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
-        private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
-    }
 }
+
+
 
 
 
